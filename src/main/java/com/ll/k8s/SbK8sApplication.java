f0f181a1d7f2589c5ee3ac016e3c6cb53549a758
황민지 <hminji4565@gmail.com>
2025-01-17 10:30:57 +0900

    설정

---
+++ src/main/java/com/ll/k8s/SbK8sApplication.java
@@ -0,0 +1,13 @@
+package com.ll.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbK8sApplication.class, args);
+    }
+
+}
